623a09dc56c49c96d4ab40bffe45091733df8cee
Suraj1213-ux <suraj24604@gmail.com>
2025-05-20 13:13:10 +0530

    fiboacci series made

--- src/com/first_java/fibonacci_series.java
+++ src/com/first_java/fibonacci_series.java
@@ -1,28 +1,33 @@
 package com.first_java;
 import java.util.Scanner;
 
-public class fibonacci_series {
-	
-	public static void printfibonacci(int n) {
-	int a = 0 ;
-	int b = 1 ;
-    System.out.print("Fibonacci Series: ");
-	
-	for (int  i= 0; i<n ; i++) {
-		System.out.print(a + "");
-		int next = a+b ;
-		a= b;
-		b= next;
-	}
-	System.out.println();
-	
-	
-		
-	}
+public class fibonacci_series{
+
+    public static void printFibonacci(int n) {
+        int a = 0, b = 1;
+        System.out.print("Fibonacci Series up to " + n + " numbers: ");
+        for (int i = 1; i <= n; i++) {
+            System.out.print(a + " ");
+            int next = a + b;
+            a = b;
+            b = next;
+        }
+        System.out.println();
+    }
+
     public static void main(String[] args) {
-    	Scanner scanner = new Scanner(System.in);
-    	int sc = scanner.nextInt();
-    	  System.out.print(sc);
-	}
-		
+    	Scanner sc= new Scanner(System.in);
+    	
+    	System.out.print("Enter how many fibonacci numbers to be print");
+    	int n = sc.nextInt();
+    	
+    	 if (n <= 0) {
+             System.out.println("Please enter a positive integer.");
+         } else {
+             printFibonacci(n);
+         }
+      
+    	 sc.close();
+    }
 }
+
