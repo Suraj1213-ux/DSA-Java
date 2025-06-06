5c6ec2e1f4e67dd0b8f335d88a75fe94b314f397
Suraj1213-ux <suraj24604@gmail.com>
2025-06-06 16:26:13 +0530

    function and methods

---
+++ src/Function_Methods/StringExample.java
@@ -0,0 +1,15 @@
+package Function_Methods;
+
+public class StringExample {
+ 
+	public static void main(String[] args) {
+		String message = greet();
+		System.out.println(message);
+				
+	}
+	
+	static String greet() {
+		String greetings = "Happy Birthday";
+		return greetings;
+	}
+}
---
+++ src/Function_Methods/arguments.java
@@ -0,0 +1,15 @@
+package Function_Methods;
+
+public class arguments {
+	public static void main(String[] args) {
+		int message = nums(20,40);
+		System.out.println(message);
+				
+	}
+	
+	static int nums(int a, int b) {
+		int sum =a +b;
+		return sum;
+	}
+
+}
