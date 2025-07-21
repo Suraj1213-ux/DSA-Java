368bc6a2c00558fc8df0dd7281d94fc571c9c2b2
Suraj1213-ux <suraj24604@gmail.com>
2025-07-21 22:33:18 +0530

    learned about data types

---
+++ src/pepcode/buildlogic.java
@@ -0,0 +1,19 @@
+package pepcode;
+
+public class buildlogic { 
+	public static void main(String[] args) {
+		
+		int x = 15 ;
+		int y = 10 ;
+		int sum = x+y  ;
+		
+		System.out.println("sum of " + x + " and " + y + " is "+  sum);
+//		System.out.println("******");
+//		System.out.println("    * ");
+//		System.out.println("  *   ");
+//		System.out.println(" *    ");
+//		System.out.println("******");
+		
+	}
+
+}
