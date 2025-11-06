a0afcafe0ba6effad43e3308e2c30d6b3ffc0452
Suraj1213-ux <suraj24604@gmail.com>
2025-11-06 23:40:17 +0530

    learned string

---
+++ src/LinearSearch/length_find.java
@@ -0,0 +1,36 @@
+package LinearSearch;
+
+public class length_find {
+	
+	public static void main(String[] args) {
+		String greet = "Suraj Sawant here.Dozo yorushuku onegaishimasu";
+		
+		String suraj = "Suraj Sawant here.Dozo yorushuku onegaishimasu";
+//		System.out.println("String:" + greet);
+		
+//		1 calculate the length of two string
+//		int length = greet.length();
+//		System.out.println("String:" + greet + " and the length of this string is " + length);
+		
+//		2 joined two strings
+//		String joinedstring = greet.concat(suraj);
+//		System.out.println(joinedstring);
+		
+//		3 comPared the two string 
+//	        1 approach 
+//		boolean result = greet.equals(suraj);
+//		System.out.println(result);
+		
+//		    2 appproach
+//		if(greet==suraj) {
+//			System.out.println(true);
+//		}else {
+//			System.out.println(false);
+//		}
+	
+	}
+ 
+	
+
+	
+}
