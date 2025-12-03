8fb0fe14c44ea95f1125a71d35c611abe4ae09c8
Suraj1213-ux <suraj24604@gmail.com>
2025-12-03 23:03:13 +0530

    ye5y

---
+++ src/practice.java
@@ -0,0 +1,173 @@
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class practice {
+
+	public static void main(String[] args) {
+		
+//		1 hello world
+//		System.out.println("hello world");
+		
+		
+		
+		
+		
+//		2 sum of two numbers
+// Scanner sc = new Scanner(System.in);
+// 
+// System.out.println("enter the value of a");
+// int a = sc.nextInt();
+// System.out.println("enter the value of b");
+// int b = sc.nextInt();
+// 
+// System.out.println(a+b);
+// sc.close();
+		
+		
+		
+ 
+//		3 even and odd
+		
+//		Scanner sc = new Scanner(System.in);
+//	  System.out.println("enter the number ");
+//	  int a = sc.nextInt();
+//	  
+//	  if (a%2==0) {
+//		  System.out.println("the number is even ");
+//		  
+//	  }
+//	  else {
+//		  System.out.println("the number is odd ");
+//	  }
+		
+		
+//		4 reverse a string 
+//		Scanner sc = new Scanner(System.in);
+//		System.out.println("enter a word");
+//		String str = sc.nextLine();
+//		String str = "automation";\
+//		StringBuilder reversed= new StringBuilder(str).reverse();
+//		
+//		System.out.println("the reverse string of " + str +" is " + reversed);
+		
+//		5 check for palindrome
+		
+//		String str = "madam";
+//		
+//		String reversed =new StringBuilder(str).reverse().toString();
+//		System.out.println(str.equals(reversed));
+		
+//		 6 fibonacci series
+		
+//		Scanner sc = new Scanner(System.in);
+//		
+//		System.out.println("enter the number");
+//		int n = sc.nextInt();
+//		int first =0;
+//		int second=1;
+//		
+//		System.out.println("the fibonaccie series of " + n + " terms is :");
+//		
+//		for(int i=1 ; i<=n ; i++ ) {
+//			
+//			System.out.print(first +",");
+//			int next = first + second;
+//			 first = second;
+//			 second = next;
+//			
+					
+//		7 factorial number 
+		
+//		int num = 5 ,
+//		factorial = 1;
+//		System.out.println("the factorial of number 5 is " );
+//		
+//		for (int i=1 ; i<=num ; i++) {
+////			System.out.print(factorial);
+//			factorial *=i;
+//		}
+//		System.out.print(factorial);
+//		
+		
+//		8 check prim number
+//		Scanner sc = new Scanner(System.in);
+//		System.out.println("enter the number");
+//		int num = sc.nextInt();
+//		
+//		boolean isPrime= true;
+//		
+//		if (num<=1) {
+//			isPrime=false;
+//		}
+//		else {
+//			for(int i=2 ; i<=num/2; i++ ) {
+//				if(num%i==0) {
+//					isPrime = false;
+//					break;
+//				}
+//			}
+//		}	
+//		if(isPrime==false) {
+//			System.out.println("the given number is not a prime number");
+//			
+//		}else {
+//			System.out.println("the given number is prime ");
+//		}
+		
+		
+//		9 sort a array 		
+//		int[]  numbers= {5,2,9,1,6};
+//		
+//		Arrays.sort(numbers);
+//		
+//		System.out.println("sorted array:" + Arrays.toString(numbers));
+//	
+//		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
